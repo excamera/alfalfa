e642235ddfeb95a7eb4baa5d79cf8d0702c211cd
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-05-10 11:22:17 -0400

    Replace references with actual storage

--- frame_state.hh
+++ frame_state.hh
@@ -5,13 +5,13 @@
 
 class FrameState {
  public:
-  FrameState(const struct vp8_frame_hdr      &  t_frame_hdr,
-             const struct vp8_segment_hdr    &  t_segment_hdr,
-             const struct vp8_loopfilter_hdr &  t_loopfilter_hdr,
-             const struct vp8_token_hdr      &  t_token_hdr,
-             const struct vp8_quant_hdr      &  t_quant_hdr,
-             const struct vp8_reference_hdr  &  t_reference_hdr,
-             const struct vp8_entropy_hdr    &  t_entropy_hdr)
+  FrameState(struct vp8_frame_hdr      t_frame_hdr,
+             struct vp8_segment_hdr    t_segment_hdr,
+             struct vp8_loopfilter_hdr t_loopfilter_hdr,
+             struct vp8_token_hdr      t_token_hdr,
+             struct vp8_quant_hdr      t_quant_hdr,
+             struct vp8_reference_hdr  t_reference_hdr,
+             struct vp8_entropy_hdr    t_entropy_hdr)
       : frame_hdr(t_frame_hdr),
         segment_hdr(t_segment_hdr),
         loopfilter_hdr(t_loopfilter_hdr),
@@ -33,13 +33,13 @@ class FrameState {
   void pretty_print_entropy_hdr(void)    const;
 
   /* Frame state */
-  const struct vp8_frame_hdr      & frame_hdr;         /* Frame scaling */
-  const struct vp8_segment_hdr    & segment_hdr;  /* Segmentation state */
-  const struct vp8_loopfilter_hdr & loopfilter_hdr; /* Loop filter state */
-  const struct vp8_token_hdr      & token_hdr;       /* # partitions and sizes */
-  const struct vp8_quant_hdr      & quant_hdr;           /* Quantizing coeffs */
-  const struct vp8_reference_hdr  & reference_hdr;   /* ref. frame */
-  const struct vp8_entropy_hdr    & entropy_hdr;       /* Probabilities */
+  const struct vp8_frame_hdr      frame_hdr;         /* Frame scaling */
+  const struct vp8_segment_hdr    segment_hdr;  /* Segmentation state */
+  const struct vp8_loopfilter_hdr loopfilter_hdr; /* Loop filter state */
+  const struct vp8_token_hdr      token_hdr;       /* # partitions and sizes */
+  const struct vp8_quant_hdr      quant_hdr;           /* Quantizing coeffs */
+  const struct vp8_reference_hdr  reference_hdr;   /* ref. frame */
+  const struct vp8_entropy_hdr    entropy_hdr;       /* Probabilities */
 };
 
 #endif  // FRAME_STATE_HH_
