13ae7aa891e02f52f11ccb2655bf4c1065a55a05
Keith Winstein <keithw@mit.edu>
2013-09-28 02:24:50 -0400

    Get the sizes

--- dx3/vp8scan.cc
+++ dx3/vp8scan.cc
@@ -25,13 +25,28 @@ int main( int argc, char *argv[] )
       Block frame = file.frame( i );
       Block tag = frame( 0, 3 );
 
-      printf( "frame %u: ", i + 1 );
-      printf( "size: %lu", frame.size() );
-      printf( ", interframe: %lu", tag.bits( 0, 1 ) );
-      printf( ", version: %lu", tag.bits( 1, 3 ) );
-      printf( ", show_frame: %lu", tag.bits( 4, 1 ) );
-      printf( ", first_partition: %lu", tag.bits( 5, 19 ) );
-      printf( "\n" );
+      const uint32_t size = frame.size();
+      const bool interframe = tag.bits( 0, 1 );
+      const uint8_t version = tag.bits( 1, 3 );
+      const bool show_frame = tag.bits( 4, 1 );
+      const uint32_t first_partition_length = tag.bits( 5, 19 );
+
+      printf( "frame %u: size: %u, interframe: %u, version: %u, show_frame: %u, size0: %u\n",
+	      i + 1, size, interframe, version, show_frame, first_partition_length );
+
+      if ( not interframe ) {
+	if ( frame( 3, 3 ).to_string() != "\x9d\x01\x2a" ) {
+	  throw Exception( argv[ 1 ], "invalid key frame header" );
+	}
+
+	Block sizes = frame( 6, 4 );
+
+	const char horizontal_scale = sizes.bits( 14, 2 ), vertical_scale = sizes.bits( 30, 2 );
+	const uint16_t width = sizes.bits( 0, 14 ), height = sizes.bits( 16, 14 );
+
+	printf( "hscale = %d, vscale = %d, width = %u, height = %u\n",
+		horizontal_scale, vertical_scale, width, height );
+      }
    }
 
   } catch ( const Exception & e ) {
