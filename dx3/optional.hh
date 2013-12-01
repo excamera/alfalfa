89fcd29891485c825fdd8c6d64d1d2d577ced8ab
Keith Winstein <keithw@mit.edu>
2013-12-01 17:24:04 -0500

    Clean up Optional class a bit

--- dx3/optional.hh
+++ dx3/optional.hh
@@ -13,26 +13,70 @@ private:
   union { T object_; bool missing_; };
 
 public:
-  Optional() : initialized_( false ), missing_( true ) {}
+  /* constructor for uninitialized optional */
+  /* missing_ field of union is necessary to get rid of "may be used uninitialized" warning */
+  Optional() : initialized_( false ), missing_() {}
+
+  /* constructor for initialized optional */
   Optional( T && other ) : initialized_( true ), object_( std::move( other ) ) {}
 
+  /* conditional constructor */
   template <typename... Targs>
   Optional( const bool is_present, Targs&&... Fargs )
-    : Optional( is_present ? Optional( T( std::forward<Targs>( Fargs )... ) ) : Optional() )
-  {}
+    : initialized_( is_present ),
+      missing_( true )
+  {
+    if ( initialized_ ) {
+      new( &object_ ) T( std::forward<Targs>( Fargs )... );
+    }
+  }
 
+  /* move constructor */
+  Optional( Optional<T> && other )
+    : initialized_( other.initialized_ ),
+      missing_( true )
+  {
+    if ( initialized_ ) {
+      new( &object_ ) T( std::move( other.object_ ) );
+    }
+  }
+
+  /* copy constructor */
+  Optional( const Optional<T> & other )
+    : initialized_( other.initialized_ ),
+      missing_( true )
+  {
+    if ( initialized_ ) {
+      new( &object_ ) T( other.object_ );
+    }
+  }
+
+  /* move assignment operator */
+  const Optional & operator=( Optional<T> && other )
+  {
+    initialized_ = other.initialized_;
+    if ( initialized_ ) {
+      object_ = std::move( other.object_ );
+    }
+    return *this;
+  }
+
+  /* copy assignment operator */
+  const Optional & operator=( const Optional<T> & other )
+  {
+    initialized_ = other.initialized_;
+    if ( initialized_ ) {
+      object_ = other.object_;
+    }
+    return *this;
+  }
+
+  /* getters */
   bool initialized( void ) const { return initialized_; }
   const T & get( void ) const { assert( initialized() ); return object_; }
   const T & get_or( const T & default_value ) const { return initialized() ? object_ : default_value; }
 
-  Optional( const Optional<T> & other ) : initialized_( other.initialized_ ), object_( other.object_ ) {}
-  Optional<T> & operator=( const Optional<T> & other )
-  {
-    initialized_ = other.initialized_;
-    object_ = other.object_;
-    return *this;
-  }
-
+  /* destructor */
   virtual ~Optional() { if ( initialized() ) { object_.~T(); } }
 };
 
