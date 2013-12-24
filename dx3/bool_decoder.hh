3de0423a48d4cf1448c68e208abb2daaedde89a6
Keith Winstein <keithw@mit.edu>
2013-12-24 01:53:35 -0500

    Allow inlining of BoolDecoder routines

--- dx3/bool_decoder.hh
+++ dx3/bool_decoder.hh
@@ -22,15 +22,74 @@ private:
   uint32_t range_, value_;
   char bit_count_;
 
-  void load_octet( void );
+  void load_octet( void )
+  {
+    if ( chunk_.size() ) {
+      value_ |= chunk_.octet();
+      chunk_ = chunk_( 1 );
+    }
+  }
 
 public:
-  BoolDecoder( const Chunk & s_chunk );
+  BoolDecoder( const Chunk & s_chunk )
+    : chunk_( s_chunk ),
+      range_( 255 ),
+      value_( 0 ),
+      bit_count_( 0 )
+  {
+    load_octet();
+    value_ <<= 8;
+    load_octet();
+  }
 
-  bool get( const Probability & probability );
+  /* based on dixie bool_decoder.h */
+  bool get( const Probability & probability )
+  {
+    const uint32_t split = 1 + (((range_ - 1) * probability) >> 8);
+    const uint32_t SPLIT = split << 8;
+    bool ret;
+
+    if ( value_ >= SPLIT ) { /* encoded a one */
+      ret = 1;
+      range_ -= split;
+      value_ -= SPLIT;
+    } else { /* encoded a zero */
+      ret = 0;
+      range_ = split;
+    }
+
+    while ( range_ < 128 ) {
+      value_ <<= 1;
+      range_ <<= 1;
+      if ( ++bit_count_ == 8 ) {
+	bit_count_ = 0;
+	load_octet();
+      }
+    }
+
+    return ret;
+  }
+
   bool bit( void ) { return get( 128 ); }
-  uint32_t uint( const unsigned int num_bits );
-  int32_t sint( const unsigned int num_bits );
+
+  uint32_t uint( const unsigned int num_bits )
+  {
+    uint32_t ret = 0;
+
+    assert( num_bits < 32 );
+
+    for ( int bit = num_bits - 1; bit >= 0; bit-- ) {
+      ret |= (BoolDecoder::bit() << bit);
+    }
+
+    return ret;
+  }
+
+  int32_t sint( const unsigned int num_bits )
+  {
+    uint32_t ret = uint( num_bits );
+    return bit() ? -ret : ret;
+  }
 
   template < uint8_t alphabet_size, class T >
   T tree( const TreeArray< alphabet_size > & nodes,
