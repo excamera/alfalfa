192af92a5b88e6c051d9254023cf6365f179728d
Keith Winstein <keithw@mit.edu>
2013-12-29 17:42:12 -0500

    loopfilter_filters.hh: Add include guards and "inline"

--- src/decoder/loopfilter_filters.hh
+++ src/decoder/loopfilter_filters.hh
@@ -1,3 +1,6 @@
+#ifndef LOOPFILTER_FILTERS_HH
+#define LOOPFILTER_FILTERS_HH
+
 /* Excerpted from libvpx/vp8/common/loopfilter_filters.c */
 
 /*
@@ -14,7 +17,7 @@
 
 typedef uint8_t uc;
 
-static int8_t vp8_signed_char_clamp(int t)
+static inline int8_t vp8_signed_char_clamp(int t)
 {
     t = (t < -128 ? -128 : t);
     t = (t > 127 ? 127 : t);
@@ -22,9 +25,9 @@ static int8_t vp8_signed_char_clamp(int t)
 }
 
 /* should we apply any filter at all ( 11111111 yes, 00000000 no) */
-signed char vp8_filter_mask(uc limit, uc blimit,
-                            uc p3, uc p2, uc p1, uc p0,
-                            uc q0, uc q1, uc q2, uc q3)
+signed inline char vp8_filter_mask(uc limit, uc blimit,
+				   uc p3, uc p2, uc p1, uc p0,
+				   uc q0, uc q1, uc q2, uc q3)
 {
     signed char mask = 0;
     mask |= (abs(p3 - p2) > limit);
@@ -38,7 +41,7 @@ signed char vp8_filter_mask(uc limit, uc blimit,
 }
 
 /* is there high variance internal edge ( 11111111 yes, 00000000 no) */
-signed char vp8_hevmask(uc thresh, uc p1, uc p0, uc q0, uc q1)
+signed inline char vp8_hevmask(uc thresh, uc p1, uc p0, uc q0, uc q1)
 {
     signed char hev = 0;
     hev  |= (abs(p1 - p0) > thresh) * -1;
@@ -46,8 +49,8 @@ signed char vp8_hevmask(uc thresh, uc p1, uc p0, uc q0, uc q1)
     return hev;
 }
 
-void vp8_filter(signed char mask, uc hev, uc & op1,
-		uc & op0, uc & oq0, uc & oq1)
+void inline vp8_filter(signed char mask, uc hev, uc & op1,
+		       uc & op0, uc & oq0, uc & oq1)
 
 {
     signed char ps0, qs0;
@@ -94,8 +97,8 @@ void vp8_filter(signed char mask, uc hev, uc & op1,
 
 }
 
-void vp8_mbfilter(signed char mask, uc hev,
-		  uc & op2, uc & op1, uc & op0, uc & oq0, uc & oq1, uc & oq2)
+void inline vp8_mbfilter(signed char mask, uc hev,
+			 uc & op2, uc & op1, uc & op0, uc & oq0, uc & oq1, uc & oq2)
 {
     signed char s, u;
     signed char filter_value, Filter1, Filter2;
@@ -150,7 +153,7 @@ void vp8_mbfilter(signed char mask, uc hev,
 }
 
 /* should we apply any filter at all ( 11111111 yes, 00000000 no) */
-static signed char vp8_simple_filter_mask(uc blimit, uc p1, uc p0, uc q0, uc q1)
+static inline signed char vp8_simple_filter_mask(uc blimit, uc p1, uc p0, uc q0, uc q1)
 {
 /* Why does this cause problems for win32?
  * error C2143: syntax error : missing ';' before 'type'
@@ -160,7 +163,7 @@ static signed char vp8_simple_filter_mask(uc blimit, uc p1, uc p0, uc q0, uc q1)
     return mask;
 }
 
-static void vp8_simple_filter(signed char mask, uc *op1, uc *op0, uc *oq0, uc *oq1)
+static inline void vp8_simple_filter(signed char mask, uc *op1, uc *op0, uc *oq0, uc *oq1)
 {
     signed char filter_value, Filter1, Filter2;
     signed char p1 = (signed char) * op1 ^ 0x80;
@@ -184,3 +187,5 @@ static void vp8_simple_filter(signed char mask, uc *op1, uc *op0, uc *oq0, uc *o
     u = vp8_signed_char_clamp(p0 + Filter2);
     *op0 = u ^ 0x80;
 }
+
+#endif /* LOOPFILTER_FILTERS_HH */
