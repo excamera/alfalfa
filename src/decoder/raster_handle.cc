4a1e2ad82d317d70299dfc69754566dd04140822
Keith Winstein <keithw@mit.edu>
2015-04-11 02:23:20 -0700

    raster_handle.cc: simplify, avoid use of unique_ptr::release

--- src/decoder/raster_handle.cc
+++ src/decoder/raster_handle.cc
@@ -1,40 +1,52 @@
 #include <memory>
 #include <queue>
 #include <functional>
+
 #include "exception.hh"
 #include "raster_handle.hh"
 
-using RasterDeleter = std::function< void( Raster * ) >;
+using namespace std;
+
+struct RasterDeleter
+{
+  void operator()( Raster * raster ) const;
+};
+
+typedef std::unique_ptr<Raster, RasterDeleter> RasterHolder;
+
+/* helper to dequeue front element from a queue */
+template <typename T>
+T dequeue( queue<T> & q )
+{
+  T ret { move( q.front() ) };
+  q.pop();
+  return ret;
+}
 
 class RasterPool
 {
 private:
-  std::queue<std::unique_ptr< Raster >> raster_pool_ {};
+  std::queue<RasterHolder> raster_pool_ {};
 
 public:
-  std::unique_ptr< Raster, RasterDeleter > make_raster( const unsigned int display_width, 
-							const unsigned int display_height )
+  RasterHolder make_raster( const unsigned int display_width,
+			    const unsigned int display_height )
   {
-    RasterDeleter deleter = [ this ]( Raster * raster )
-			    {
-  			      this->free_raster( raster );
-  			    };
-
     if ( raster_pool_.empty() ) {
-      return std::unique_ptr< Raster, RasterDeleter >( new Raster( display_width, display_height ), deleter );
+      return RasterHolder( new Raster( display_width, display_height ) );
     }
-    else {
-      if ( raster_pool_.front()->display_width() != display_width || raster_pool_.front()->display_height() != display_height ) {
-	throw Unsupported( "rasters with different dimensions" );
-      }
-      std::unique_ptr< Raster, RasterDeleter > raster_ptr( raster_pool_.front().release(), deleter );
-      raster_pool_.pop();
-      return raster_ptr;
+
+    if ( (raster_pool_.front()->display_width() != display_width)
+	 or (raster_pool_.front()->display_height() != display_height) ) {
+      throw Unsupported( "raster size has changed" );
     }
+
+    return dequeue( raster_pool_ );
   }
+
   void free_raster( Raster * raster )
   {
-    raster_pool_.push( std::unique_ptr< Raster >( raster ) );
+    raster_pool_.emplace( raster );
   }
 };
 
@@ -44,6 +56,11 @@ static RasterPool & global_raster_pool( void )
   return pool;
 }
 
+void RasterDeleter::operator()( Raster * raster ) const
+{
+  global_raster_pool().free_raster( raster );
+}
+
 RasterHandle::RasterHandle( const unsigned int display_width, const unsigned int display_height )
-  : raster_( std::move( global_raster_pool().make_raster( display_width, display_height ) ) )
+  : raster_( global_raster_pool().make_raster( display_width, display_height ) )
 {}
