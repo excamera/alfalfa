d04eb28e0bc51dd13ebd8cef3dd6a184611364a9
Deepak Narayanan <deepakn94@gmail.com>
2015-10-31 14:42:44 -0700

    Move method definitions from .hh file to .cc file -- second phase of two phase refactor

--- src/format/manifests.hh
+++ src/format/manifests.hh
@@ -48,32 +48,9 @@ public:
     RasterListCollection, RasterListSequencedTag>( filename, magic_number, mode )
   {}
 
-  bool has( const size_t & raster_hash ) const
-  {
-    const RasterListCollectionByHash & data_by_hash = collection_.get<RasterListByHashTag>();
-    return data_by_hash.count( raster_hash ) > 0;
-  }
-
-  bool operator==( const RasterList & other ) const
-  {
-    if ( collection_.get<RasterListByHashTag>().size() !=
-         other.collection_.get<RasterListByHashTag>().size() ) {
-      return false;
-    }
-
-    for ( auto const & item : collection_.get<RasterListByHashTag>() ) {
-      if ( not other.has( item.hash ) ) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  bool operator!=( const RasterList & other ) const
-  {
-    return not ( ( *this ) == other );
-  }
+  bool has( const size_t & raster_hash ) const;
+  bool operator==( const RasterList & other ) const;
+  bool operator!=( const RasterList & other ) const;
 };
 
 /*
@@ -193,56 +170,10 @@ public:
   {}
 
   std::pair<TrackDBCollectionByIds::iterator, TrackDBCollectionByIds::iterator>
-  search_by_track_id( const size_t track_id )
-  {
-    TrackDBCollectionByIds & index = collection_.get<TrackDBByIdsTag>();
-    // Only specify the first field, since our ordered_index is sorted
-    // first by track ID and then by frame ID
-    return index.equal_range( track_id );
-  }
-
+  search_by_track_id( const size_t track_id );
   Optional<TrackData>
-  search_next_frame( const size_t track_id, const SourceHash source_hash, const TargetHash target_hash )
-  {
-    TrackDBCollectionByFrameAndTrackId & hashed_index = collection_.get<TrackDBByFrameAndTrackIdTag>();
-    boost::tuple<size_t, SourceHash, TargetHash> hashed_key =
-      boost::make_tuple( track_id, source_hash, target_hash );
-    if ( hashed_index.count( hashed_key ) == 0 )
-      return Optional<TrackData>();
-    TrackDBCollectionByFrameAndTrackId::iterator frame_data = hashed_index.find(
-      hashed_key );
-    // Find the frame_id of associated frame
-    size_t frame_id = frame_data->frame_id;
-
-    TrackDBCollectionByIds & ordered_index = collection_.get<TrackDBByIdsTag>();
-    boost::tuple<size_t, size_t> ordered_key =
-      boost::make_tuple( track_id, frame_id+1 );
-    if ( ordered_index.count( ordered_key ) == 0)
-      return Optional<TrackData>();
-    TrackDBCollectionByIds::iterator ids_iterator = ordered_index.find(
-      ordered_key);
-    return make_optional( true, *ids_iterator );
-  }
-
-  void merge( const TrackDB & db )
-  {
-    TrackDBCollectionByIds & track_ids = collection_.get<TrackDBByIdsTag>();
-    map<size_t, size_t> new_track_ids;
-
-    for ( auto item : db.collection_.get<TrackDBSequencedTag>() ) {
-      if ( new_track_ids.count( item.track_id ) > 0 ) {
-        item.track_id = new_track_ids[ item.track_id ];
-      }
-      else if ( track_ids.count( item.track_id ) > 0 ) {
-        size_t new_track_id = item.track_id;
-        while ( track_ids.count ( ++new_track_id ) > 0 );
-        new_track_ids[ item.track_id ] = new_track_id;
-        item.track_id = new_track_id;
-      }
-
-      insert( item );
-    }
-  }
+  search_next_frame( const size_t track_id, const SourceHash source_hash, const TargetHash target_hash );
+  void merge( const TrackDB & db );
 };
 
 #endif /* MANIFESTS_HH */
