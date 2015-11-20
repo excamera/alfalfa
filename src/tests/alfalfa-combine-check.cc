7054007ae44f83d0bae06bbfa416e6e729cbb6e3
Deepak Narayanan <deepakn94@gmail.com>
2015-11-19 22:36:56 -0800

    Refactored combine check, added combine + encode test, renamed test files

--- src/tests/alfalfa-combine-check.cc
+++ src/tests/alfalfa-combine-check.cc
@@ -6,138 +6,95 @@
 
 using namespace std;
 
-/* Simple combine test. Imports the same video twice, and combines them.
-   Checks to make sure entries in the raster list, quality db and frame db
-   aren't replicated. Also checks to make sure track db has two distinct tracks. */
-int alfalfa_combine_test_double_import( string ivf_file_path, string alfalfa_video_dir ) {
-  bool found;
-
+/* Combine test checker. Ensures that the combined video has the right entries
+   in the raster list, quality db and frame db. */
+void alfalfa_combine_test_check( string alfalfa_video_dir, string alfalfa_video_combined_dir, size_t combined_track_id ) {
   PlayableAlfalfaVideo alfalfa_video( alfalfa_video_dir );
+  PlayableAlfalfaVideo alfalfa_video_combined( alfalfa_video_combined_dir );
 
-  RasterList & raster_list = alfalfa_video.raster_list();
-  QualityDB & quality_db = alfalfa_video.quality_db();
-  FrameDB & frame_db = alfalfa_video.frame_db();
-
-  TrackingPlayer player( ivf_file_path );
-  size_t frame_index = 0;
-
-  while ( not player.eof() ) {
-    FrameInfo next_frame( player.serialize_next().first );
-
-    const size_t raster = next_frame.target_hash().output_hash;
-    const size_t approximate_raster = raster;
-
-    if ( next_frame.target_hash().shown ) {
-      // Check raster_list
-      if ( not raster_list.has( raster ) )
-        return 1;
-
-      // Check quality db
-      found = false;
-      auto it_orig =
-        quality_db.search_by_original_raster( raster );
-      QualityDBCollectionByOriginalRaster::iterator it_begin_orig = it_orig.first;
-      QualityDBCollectionByOriginalRaster::iterator it_end_orig = it_orig.second;
-      while ( it_begin_orig != it_end_orig ) {
-        if ( it_begin_orig->original_raster != raster )
-          return 1;
-        if ( it_begin_orig->approximate_raster == approximate_raster ) {
-          if ( it_begin_orig->quality == 1.0 )
-            found = true;
-        }
-        it_begin_orig++;
-      }
-      if ( !found )
-        return 1;
-
-      found = false;
-      auto it_approx =
-        quality_db.search_by_approximate_raster( approximate_raster );
-      QualityDBCollectionByApproximateRaster::iterator it_begin_approx = it_approx.first;
-      QualityDBCollectionByApproximateRaster::iterator it_end_approx = it_approx.second;
-      while ( it_begin_approx != it_end_approx ) {
-        if ( it_begin_approx->approximate_raster != approximate_raster )
-          return 1;
-        if ( it_begin_approx->original_raster == raster ) {
-          if ( it_begin_approx->quality == 1.0 )
-            found = true;
-        }
-        it_begin_approx++;
-      }
-      if ( !found )
-        return 1;
-    }
-
-    // Check frame db
-    found = false;
-    auto it_output_hash =
-      frame_db.search_by_output_hash( raster );
-    FrameDataSetCollectionByOutputHash::iterator it_begin_output_hash = it_output_hash.first;
-    FrameDataSetCollectionByOutputHash::iterator it_end_output_hash = it_output_hash.second;
-    while ( it_begin_output_hash != it_end_output_hash ) {
-      if ( it_begin_output_hash->source_hash() == next_frame.source_hash() and
-           it_begin_output_hash->target_hash() == next_frame.target_hash() )
-        found = true;
-      it_begin_output_hash++;
-    }
-    if ( !found )
-      return 1;
-
-    // Check track db
-    bool found;
-
-    found = false;
-    pair<TrackDBIterator, TrackDBIterator> it_ids1 =
-      alfalfa_video.get_frames( 0 );
-    TrackDBIterator it_begin_ids1 = it_ids1.first;
-    TrackDBIterator it_end_ids1 = it_ids1.second;
-    size_t local_frame_index1 = 0;
-    while ( it_begin_ids1 != it_end_ids1 ) {
-      FrameInfo frame_info = *it_begin_ids1;
-      if ( local_frame_index1 == frame_index and
-           frame_info.source_hash() == next_frame.source_hash() and
-           frame_info.target_hash() == next_frame.target_hash() )
-        found = true;
-      local_frame_index1++;
-      it_begin_ids1++;
-    }
-    if ( !found ) {
-      return 1;
-    }
-
-    found = false;
-    pair<TrackDBIterator, TrackDBIterator> it_ids2 =
-      alfalfa_video.get_frames( 1 );
-    TrackDBIterator it_begin_ids2 = it_ids2.first;
-    TrackDBIterator it_end_ids2 = it_ids2.second;
-    size_t local_frame_index2 = 0;
-    while ( it_begin_ids2 != it_end_ids2 ) {
-      FrameInfo frame_info = *it_begin_ids2;
-      if ( local_frame_index2 == frame_index and
-           frame_info.source_hash() == next_frame.source_hash() and
-           frame_info.target_hash() == next_frame.target_hash() )
-        found = true;
-      local_frame_index2++;
-      it_begin_ids2++;
-    }
-    if ( !found ) {
-      return 1;
-    }
-
-    frame_index++;
+  // First, make sure that the raster lists are identical
+  RasterList raster_list = alfalfa_video.raster_list();
+  RasterList raster_list_combined = alfalfa_video_combined.raster_list();
+  for ( auto it = raster_list.begin(); it != raster_list.end(); it++ ) {
+    if ( not raster_list_combined.has( it->hash ) )
+      throw invalid_argument( "raster lists don't match!" );
+  }
+  for ( auto it = raster_list_combined.begin(); it != raster_list_combined.end(); it++ ) {
+    if ( not raster_list.has( it->hash ) )
+      throw invalid_argument( "raster lists don't match!" );
   }
 
-  return 0;
+  // Now, check the quality db
+  QualityDB quality_db = alfalfa_video.quality_db();
+  QualityDB quality_db_combined = alfalfa_video_combined.quality_db();
+  for ( auto it = quality_db.begin(); it != quality_db.end(); it++ ) {
+    bool found;
+    found = false;
+    auto original_results = quality_db_combined.search_by_original_raster( it->original_raster );
+    while ( original_results.first != original_results.second ) {
+      if ( original_results.first->approximate_raster == it->approximate_raster and
+           original_results.first->quality == it->quality )
+        found = true;
+      original_results.first++;
+    }
+    if ( not found )
+      throw logic_error( "quality_data not found in combined video." );
+
+    found = false;
+    auto approx_results = quality_db_combined.search_by_approximate_raster( it->approximate_raster );
+    while ( approx_results.first != approx_results.second ) {
+      if ( approx_results.first->original_raster == it->original_raster and
+           approx_results.first->quality == it->quality )
+        found = true;
+      approx_results.first++;
+    }
+    if ( not found )
+      throw logic_error( "quality_data not found in combined video. (2)" );
+  }
+
+  // Now check the frame db
+  FrameDB frame_db = alfalfa_video.frame_db();
+  FrameDB frame_db_combined = alfalfa_video_combined.frame_db();
+  auto frames = alfalfa_video.get_frames();
+  for ( auto it = frames.first; it != frames.second; it++ ) {
+    if ( not frame_db_combined.has_frame_name( it->name() ) )
+      throw logic_error( "frame missing" );
+  }
+
+  // Now check the track db
+  auto track_frames = alfalfa_video.get_frames( 0 );
+  auto track_frames_combined = alfalfa_video_combined.get_frames( combined_track_id );
+  while ( track_frames.first != track_frames.second ||
+          track_frames_combined.first != track_frames_combined.second ) {
+    if ( not ( track_frames.first->source_hash() == track_frames_combined.first->source_hash() &&
+               track_frames.first->target_hash() == track_frames_combined.first->target_hash() ) ) {
+      throw logic_error( "tracks differ" );
+    }
+    track_frames.first++;
+    track_frames_combined.first++;
+  }
+  if ( not ( ( track_frames.first == track_frames.second ) and
+             ( track_frames_combined.first == track_frames_combined.second ) ) )
+    throw logic_error( "tracks are of different length" );
 }
 
 int main(int argc, char const *argv[]) {
-  if ( argc != 3 ) {
-    cerr << "usage: combine-test <ivf-file> <alfalfa-video-dir>" << endl;
-    return 1;
+  try {
+    if ( argc != 4 ) {
+      cerr << "usage: combine-test <alfalfa-video-dir1> <alfalfa-video-dir2> <alfalfa-video-dir-combined>" << endl;
+      return EX_USAGE;
+    }
+
+    string alfalfa_video_dir1( argv[ 1 ] );
+    string alfalfa_video_dir2( argv[ 2 ] );
+    string alfalfa_video_dir_combined( argv[ 3 ] );
+
+    alfalfa_combine_test_check( alfalfa_video_dir1, alfalfa_video_dir_combined, 0 );
+    alfalfa_combine_test_check( alfalfa_video_dir2, alfalfa_video_dir_combined, 1 );
+  } catch (const exception &e ) {
+    print_exception( argv[ 0 ], e );
+    return EXIT_FAILURE;
   }
 
-  string ivf_file_path( argv[ 1 ] );
-  string alfalfa_video_dir( argv[ 2 ] );
-
-  return alfalfa_combine_test_double_import( ivf_file_path, alfalfa_video_dir );
+  return EXIT_SUCCESS;
 }
