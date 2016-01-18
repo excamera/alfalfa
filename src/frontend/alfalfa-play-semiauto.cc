9bd868e88a3a9a4a1fc998ac7dd5fc3e779fe3fa
Keith Winstein <keithw@cs.stanford.edu>
2016-01-18 00:57:38 -0800

    Fill out alfalfa-play-semiauto.cc a little more

--- src/frontend/alfalfa-play-semiauto.cc
+++ src/frontend/alfalfa-play-semiauto.cc
@@ -3,6 +3,9 @@
 
 #include <sysexits.h>
 
+#include "alfalfa_video_client.hh"
+#include "display.hh"
+
 using namespace std;
 
 int main( const int argc, char const *argv[] )
@@ -12,7 +15,35 @@ int main( const int argc, char const *argv[] )
     return EX_USAGE;
   }
 
-  const string server_address( argv[ 1 ] );
+  const AlfalfaVideoClient video { argv[ 1 ] };
+  Decoder decoder( video.get_video_width(), video.get_video_height() );
+  VideoDisplay display { decoder.example_raster() };
 
+  /* print out the available tracks in the video */
+  const vector<size_t> track_ids = video.get_track_ids();
+
+  cout << "track_ids:";
+  for ( const auto & id : track_ids ) {
+    cout << " " << id;
+  }
+  cout << "\n";
+
+  /* play the first track found */
+  if ( track_ids.empty() ) {
+    throw runtime_error( "no tracks to play" );
+  }
+  
+  const size_t track_to_play = track_ids.front();
+  const size_t track_size = video.get_track_size( track_to_play );
+  const vector<FrameInfo> frames = video.get_frames( track_to_play, 0, track_size );
+
+  for ( const auto & frame : frames ) {
+    const string coded_frame = video.get_chunk( frame );
+    const Optional<RasterHandle> raster = decoder.parse_and_decode_frame( coded_frame );
+    if ( raster.initialized() ) {
+      display.draw( raster.get() );
+    }
+  }
+  
   return EXIT_SUCCESS;
 }
