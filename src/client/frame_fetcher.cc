dafb793402641d2845bcce939e5d78d5d83e73db
Keith Winstein <keithw@cs.stanford.edu>
2016-01-20 00:20:27 -0800

    Compile with basic use of cppnetlib

--- src/client/frame_fetcher.cc
+++ src/client/frame_fetcher.cc
@@ -1,13 +1,29 @@
 #include "frame_fetcher.hh"
 
+#include <boost/network/protocol/http/client.hpp>
+
 using namespace std;
+using namespace boost::network::http;
+
+struct HTTPClientWrapper
+{
+  client c {};
+};
 
 FrameFetcher::FrameFetcher( const string & framestore_url )
   : framestore_url_( framestore_url ),
-    http_client_()
+    http_client_( new HTTPClientWrapper )
 {}
 
-string FrameFetcher::get_chunk( const FrameInfo & )
+string FrameFetcher::get_chunk( const FrameInfo & frame_info )
 {
-  return "";
+  client::request the_request;
+  the_request << boost::network::destination( framestore_url_ );
+  const string range_beginning = to_string( frame_info.offset() );
+  const string range_end = to_string( frame_info.offset() + frame_info.length() - 1 );
+  the_request << boost::network::header( "Range", "bytes=" + range_beginning + "-" + range_end );
+  client::response the_response = http_client_->c.get( the_request );
+  return body( the_response );
 }
+
+FrameFetcher::~FrameFetcher() {}
--- src/client/frame_fetcher.hh
+++ src/client/frame_fetcher.hh
@@ -2,19 +2,22 @@
 #define FRAME_FETCHER_HH
 
 #include <string>
-#include <boost/network/protocol/http/client.hpp>
+#include <memory>
 
 #include "frame_info.hh"
+
+class HTTPClientWrapper;
 
 class FrameFetcher
 {
 private:
   std::string framestore_url_;
-  boost::network::http::client http_client_;
+  std::unique_ptr<HTTPClientWrapper> http_client_;
   
 public:
   FrameFetcher( const std::string & framestore_url );
-
+  ~FrameFetcher();
+  
   std::string get_chunk( const FrameInfo & frame_info );
 };
 
