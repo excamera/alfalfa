3712a0f363a83f29c3c10b922f5794b3bf5e32b2
Keith Winstein <keithw@cs.stanford.edu>
2016-01-19 22:31:18 -0800

    Start of FrameFetcher

---
+++ src/client/frame_fetcher.cc
@@ -0,0 +1,7 @@
+#include "frame_fetcher.hh"
+
+using namespace std;
+
+FrameFetcher::FrameFetcher( const string & , const string & )
+{
+}
---
+++ src/client/frame_fetcher.hh
@@ -0,0 +1,12 @@
+#ifndef FRAME_FETCHER_HH
+#define FRAME_FETCHER_HH
+
+#include <string>
+
+class FrameFetcher
+{
+public:
+  FrameFetcher( const std::string & host, const std::string & port );
+};
+
+#endif /* FRAME_FETCHER_HH */
