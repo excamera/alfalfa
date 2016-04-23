ab11c4fa27a7160fba3019cb62b370a5c044f432
Keith Winstein <keithw@cs.stanford.edu>
2016-04-22 17:32:36 -0700

    Display a frame

--- src/frontend/dumbencode.cc
+++ src/frontend/dumbencode.cc
@@ -2,14 +2,13 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <ivf_writer.cc>
 
-#include "encoder.hh"
 #include "dct.hh"
 #include "frame.hh"
 #include "player.hh"
 #include "vp8_raster.hh"
 #include "decoder.hh"
+#include "display.hh"
 
 using namespace std;
 
@@ -27,43 +26,31 @@ int main( int argc, char *argv[] )
       abort();
     }
 
-    if ( argc != 2 ) {
-      cerr << "Usage: " << argv[ 0 ] << "[INPUT_VIDEO]" << endl;
+    if ( argc != 3 ) {
+      cerr << "Usage: " << argv[ 0 ] << " <video> <frame number>" << endl;
       return EXIT_FAILURE;
     }
 
     IVF ivf( argv[ 1 ] );
-    Player player( argv[ 1 ] );
-    DecoderState decoder_state( ivf.width(), ivf.height() );
+    unsigned int frame_number = atoi( argv[ 2 ] );
 
-    int frame_number = 3;
-
-    for ( int i = 0; i < frame_number; i++ ) {
-      UncompressedChunk whole_frame( ivf.frame( i ), ivf.width(), ivf.height() );
-      if ( whole_frame.key_frame() ) {
-        decoder_state.parse_and_apply<KeyFrame>( whole_frame );
-      }
-      else {
-        decoder_state.parse_and_apply<InterFrame>( whole_frame );
-      }
-
-      player.advance();
+    UncompressedChunk uncompressed_chunk { ivf.frame( frame_number ), ivf.width(), ivf.height() };
+    if ( not uncompressed_chunk.key_frame() ) {
+      throw runtime_error( "not a keyframe" );
     }
 
+    DecoderState state { ivf.width(), ivf.height() };
+    References references { ivf.width(), ivf.height() };
+    MutableRasterHandle raster { ivf.width(), ivf.height() };
 
-    UncompressedChunk whole_frame( ivf.frame( frame_number ), ivf.width(), ivf.height() );
+    KeyFrame frame = state.parse_and_apply<KeyFrame>( uncompressed_chunk );
+    frame.decode( state.segmentation, references, raster );
+    frame.loopfilter( state.segmentation, state.filter_adjustments, raster );
 
-    if ( not whole_frame.key_frame() ) {
-      throw domain_error("not a keyframe.");
-    }
+    VideoDisplay display { raster };
+    display.draw( raster );
 
-    KeyFrame frame = decoder_state.parse_and_apply<KeyFrame>( whole_frame );
-    RasterHandle raster_handle = player.advance();
-    const VP8Raster & raster = raster_handle.get();
-
-    Encoder encoder;
-    // ....
-    encoder.encode( raster, frame );
+    pause();
   } catch ( const exception & e ) {
     print_exception( argv[ 0 ], e );
     return EXIT_FAILURE;
