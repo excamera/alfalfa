019d715deab0cd3fbf2728722aee4ccc4df261ed
Keith Winstein <keithw@cs.stanford.edu>
2016-12-30 17:12:27 -0800

    Add fake-webcam.cc (parcels out frames at a specified rate)

---
+++ src/frontend/fake-webcam.cc
@@ -0,0 +1,71 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+#include "yuv4mpeg.hh"
+
+using namespace std;
+
+void usage( const char *argv0 )
+{
+  cerr << "Usage: " << argv0 << " INPUT FPS" << endl;
+}
+
+unsigned int paranoid_atoi( const string & in )
+{
+  const unsigned int ret = stoul( in );
+  const string roundtrip = to_string( ret );
+  if ( roundtrip != in ) {
+    throw runtime_error( "invalid unsigned integer: " + in );
+  }
+  return ret;
+}
+
+int main( int argc, char *argv[] )
+{
+  /* check the command-line arguments */
+  if ( argc < 1 ) { /* for sticklers */
+    abort();
+  }
+
+  if ( argc != 3 ) {
+    usage( argv[ 0 ] );
+    return EXIT_FAILURE;
+  }
+
+  /* open the YUV4MPEG input */
+  YUV4MPEGReader input { argv[ 1 ] };
+
+  /* parse the # of frames per second of playback */
+  unsigned int frames_per_second = paranoid_atoi( argv[ 2 ] );
+
+  /* open the output */
+  FileDescriptor stdout { STDOUT_FILENO };
+
+  const auto interval_between_frames = chrono::microseconds( int( 1.0e6 / frames_per_second ) );
+  
+  auto next_frame_is_due = chrono::system_clock::now();
+
+  bool initialized = false;
+  while ( true ) {
+    /* wait until next frame is due */
+    this_thread::sleep_until( next_frame_is_due );
+    next_frame_is_due += interval_between_frames;
+
+    /* get the next frame to send */
+    const Optional<RasterHandle> raster = input.get_next_frame();
+    if ( not raster.initialized() ) {
+      break; /* eof */
+    }
+
+    /* send the file header if we haven't already */
+    if ( not initialized ) {
+      stdout.write( YUV4MPEGHeader( raster.get() ).to_string() );
+    }
+
+    /* send the frame */
+    YUV4MPEGFrameWriter::write( raster.get(), stdout );
+  }
+}
