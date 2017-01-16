76e342788c2b2dd2a608cf269b94559be6401f33
Keith Winstein <keithw@cs.stanford.edu>
2017-01-15 22:41:43 -0800

    Add frame_pool.hh/cc

---
+++ src/decoder/frame_pool.cc
@@ -0,0 +1,108 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include <queue>
+#include <memory>
+
+#include "exception.hh"
+#include "frame_pool.hh"
+
+using namespace std;
+
+/* helper to dequeue front element from a queue */
+template <typename T>
+static T dequeue( queue<T> & q )
+{
+  T ret { move( q.front() ) };
+  q.pop();
+  return ret;
+}
+
+template<class FrameType>
+class FramePool
+{
+public:
+  typedef std::unique_ptr<FrameType, FrameDeleter<FrameType>> FrameHolder;
+
+private:
+  queue<FrameHolder> unused_frames_ {};
+
+  mutex mutex_ {};
+
+public:
+  FrameHolder make_frame( const uint16_t width,
+                          const uint16_t height )
+  {
+    unique_lock<mutex> lock { mutex_ };
+
+    FrameHolder ret;
+
+    if ( unused_frames_.empty() ) {
+      ret.reset( new FrameType( width, height ) );
+    } else {
+      if ( (unused_frames_.front()->display_width() != width )
+           or (unused_frames_.front()->display_height() != height ) ) {
+        throw Unsupported( "raster size has changed" );
+      } else {
+        ret = dequeue( unused_frames_ );
+      }
+    }
+
+    ret.get_deleter().set_frame_pool( this );
+
+    return ret;
+  }
+
+  void free_frame( FrameType * frame )
+  {
+    unique_lock<mutex> lock { mutex_ };
+
+    assert( frame );
+    unused_frames_.emplace( frame );
+  }
+};
+
+template<class FrameType>
+void FrameDeleter<FrameType>::operator()( FrameType * frame ) const
+{
+  if ( frame_pool_ ) {
+    frame_pool_->free_frame( frame );
+  } else {
+    delete frame;
+  }
+}
+
+template<class FrameType>
+FramePool<FrameType> * FrameDeleter<FrameType>::get_frame_pool() const
+{
+  return frame_pool_;
+}
+
+template<class FrameType>
+void FrameDeleter<FrameType>::set_frame_pool( FramePool<FrameType> * pool )
+{
+  assert( not frame_pool_ );
+  frame_pool_ = pool;
+}
+
+template<class FrameType>
+static FramePool<FrameType> & global_frame_pool()
+{
+  static FramePool<FrameType> pool;
+  return pool;
+}
+
+template<class FrameType>
+FrameHandle<FrameType>::FrameHandle( const uint16_t width,
+                                     const uint16_t height )
+  : FrameHandle( width, height, global_frame_pool<FrameType>() )
+{}
+
+template<class FrameType>
+FrameHandle<FrameType>::FrameHandle( const uint16_t width,
+                                     const uint16_t height,
+                                     FramePool<FrameType> & frame_pool )
+  : frame_( frame_pool.make_frame( width, height ) )
+{}
+
+template class FrameHandle<KeyFrame>;
+template class FrameHandle<InterFrame>;
---
+++ src/decoder/frame_pool.hh
@@ -0,0 +1,44 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef FRAME_POOL_HH
+#define FRAME_POOL_HH
+
+#include "frame.hh"
+
+template <class FrameType> class FramePool;
+
+template<class FrameType>
+class FrameDeleter
+{
+private:
+  FramePool<FrameType> * frame_pool_ = nullptr;
+
+public:
+  void operator()( FrameType * frame ) const;
+
+  FramePool<FrameType> * get_frame_pool( void ) const;
+  void set_frame_pool( FramePool<FrameType> * pool );
+};
+
+template<class FrameType>
+class FrameHandle
+{
+private:
+  std::unique_ptr<FrameType, FrameDeleter<FrameType>> frame_;
+
+public:
+  FrameHandle( const uint16_t width, const uint16_t height );
+  FrameHandle( const uint16_t width, const uint16_t height,
+	       FramePool<FrameType> & frame_pool );
+
+  operator const FrameType & () const { return *frame_; }
+  operator FrameType & () { return *frame_; }
+
+  const FrameType & get( void ) const { return *frame_; }
+  FrameType & get( void ) { return *frame_; }
+};
+
+using KeyFrameHandle = FrameHandle<KeyFrame>;
+using InterFrameHandle = FrameHandle<InterFrame>;
+
+#endif /* FRAME_POOL_HH */
