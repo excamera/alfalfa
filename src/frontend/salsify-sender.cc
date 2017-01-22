bed810f37ce71b0e72d8613e518e610cd21417a5
Sadjad Fouladi <sfouladi@gmail.com>
2017-01-22 01:35:04 -0800

    Change the worst quantizer to 96.

--- src/frontend/salsify-sender.cc
+++ src/frontend/salsify-sender.cc
@@ -374,14 +374,17 @@ int main( int argc, char *argv[] )
       /* try various quantizers */
       encode_jobs.emplace_back( "same", raster, encoder, CONSTANT_QUANTIZER, last_quantizer, 0 );
 
+      encode_jobs.emplace_back( "improvealittle", raster, encoder, CONSTANT_QUANTIZER,
+                                increment_quantizer( last_quantizer, -2 ), 0 );
+
       encode_jobs.emplace_back( "improve", raster, encoder, CONSTANT_QUANTIZER,
                                 increment_quantizer( last_quantizer, -5 ), 0 );
 
       encode_jobs.emplace_back( "improvemore", raster, encoder, CONSTANT_QUANTIZER,
                                 increment_quantizer( last_quantizer, -11 ), 0 );
 
-      encode_jobs.emplace_back( "improvemuchmore", raster, encoder, CONSTANT_QUANTIZER,
-                                increment_quantizer( last_quantizer, -29 ), 0 );
+      // encode_jobs.emplace_back( "improvemuchmore", raster, encoder, CONSTANT_QUANTIZER,
+      //                           increment_quantizer( last_quantizer, -29 ), 0 );
 
       encode_jobs.emplace_back( "worsen", raster, encoder, CONSTANT_QUANTIZER,
                                 increment_quantizer( last_quantizer, +11 ), 0 );
@@ -395,7 +398,7 @@ int main( int argc, char *argv[] )
       /*encode_jobs.emplace_back( "worsenalotmore", raster, encoder, CONSTANT_QUANTIZER,
                                 increment_quantizer( last_quantizer, +51 ), 0 ); */
 
-      encode_jobs.emplace_back( "fail-small", raster, encoder, CONSTANT_QUANTIZER, 127, 0 );
+      encode_jobs.emplace_back( "fail-small", raster, encoder, CONSTANT_QUANTIZER, 96, 0 );
 
       // this thread will spawn all the encoding jobs and will wait on the results
       thread(
