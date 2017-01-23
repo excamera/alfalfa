20dc71de417a07f5da39728e2a3f1c7fae1ef37b
Sadjad Fouladi <sfouladi@gmail.com>
2017-01-22 19:06:59 -0800

    Add fullscreen option to real-webcam.

--- src/frontend/real-webcam.cc
+++ src/frontend/real-webcam.cc
@@ -14,7 +14,7 @@ using namespace std;
 void usage( const char *argv0 )
 {
   cerr << "Usage: " << argv0
-       << " [-d, --device CAMERA] [-p, --pixfmt PIXEL_FORMAT]"
+       << " [-d, --device CAMERA] [-p, --pixfmt PIXEL_FORMAT] [-f, --fullscreen]"
        << endl;
 }
 
@@ -28,39 +28,35 @@ int main( int argc, char *argv[] )
   /* camera settings */
   string camera_device = "/dev/video0";
   string pixel_format = "NV12";
+  bool fullscreen = false;
 
   const option command_line_options[] = {
-    { "device", required_argument, nullptr, 'd' },
-    { "pixfmt", required_argument, nullptr, 'p' },
+    { "device",     required_argument, nullptr, 'd' },
+    { "pixfmt",     required_argument, nullptr, 'p' },
+    { "fullscreen", no_argument,       nullptr, 'f' },
     { 0, 0, 0, 0 }
   };
 
   while ( true ) {
-    const int opt = getopt_long( argc, argv, "d:p:", command_line_options, nullptr );
+    const int opt = getopt_long( argc, argv, "d:p:f", command_line_options, nullptr );
 
     if ( opt == -1 ) {
       break;
     }
 
     switch ( opt ) {
-    case 'd':
-      camera_device = optarg;
-      break;
+    case 'd': camera_device = optarg; break;
+    case 'p': pixel_format = optarg; break;
+    case 'f': fullscreen = true; break;
 
-    case 'p':
-      pixel_format = optarg;
-      break;
-
-    default:
-      usage( argv[ 0 ] );
-      return EXIT_FAILURE;
+    default: usage( argv[ 0 ] ); return EXIT_FAILURE;
     }
   }
 
   Camera camera { 1280, 720, PIXEL_FORMAT_STRS.at( pixel_format ), camera_device };
 
   RasterHandle r { MutableRasterHandle{ 1280, 720 } };
-  VideoDisplay display { r };
+  VideoDisplay display { r, fullscreen };
 
   while ( true ) {
     auto raster = camera.get_next_frame();
