b96ab983f4b21eb8b9b46e31560f2ca076e32c39
Sadjad Fouladi <sfouladi@gmail.com>
2018-01-30 16:31:48 -0800

    Create procinfo.

---
+++ src/util/procinfo.cc
@@ -0,0 +1,15 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include "procinfo.hh"
+
+#include <fstream>
+
+using namespace std;
+
+size_t procinfo::memory_usage()
+{
+  ifstream fin( "/proc/self/statm" );
+  size_t v;
+  fin >> v;
+  return v;
+}
---
+++ src/util/procinfo.hh
@@ -0,0 +1,13 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef PROCINFO_HH
+#define PROCINFO_HH
+
+#include <cstdlib>
+
+namespace procinfo
+{
+  size_t memory_usage();
+}
+
+#endif /* PROCINFO_HH */
